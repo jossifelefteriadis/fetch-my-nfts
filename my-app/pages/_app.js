40475b4b97a43636eac18938ef6655f8dbb245ab
jossif elefteriadis <jossif_@hotmail.com>
2022-09-11 20:55:18 +0200

    Create next app, add logo and rainbowkit

---
+++ my-app/pages/_app.js
@@ -0,0 +1,44 @@
+import "../styles/globals.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [
+    chain.mainnet,
+    chain.goerli,
+    chain.rinkeby,
+  ],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Fetch My NFTs",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider theme={darkTheme()} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
---
+++ my-app/pages/index.js
@@ -0,0 +1,36 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import styles from "../styles/Home.module.css";
+import Nav from "../components/nav";
+import { useAccount } from "wagmi";
+import { useEffect, useState } from "react";
+
+import LoggedIn from "../components/loggedIn";
+
+export default function Home() {
+  const { isConnected } = useAccount();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (!isConnected) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [isConnected]);
+  return (
+    <section className={styles.container}>
+      <Nav />
+      {isLoggedIn ? (
+        <main className={styles.main}>
+          <h1 className={styles.title} style={{ marginBottom: "4rem" }}>
+            Connect your Metamask and see your <span>LW3</span> and{" "}
+            <span>Buildspace</span> NFT&apos;s
+          </h1>
+          <ConnectButton />
+        </main>
+      ) : (
+        <LoggedIn />
+      )}
+    </section>
+  );
+}
