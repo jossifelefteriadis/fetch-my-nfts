a12dcbff8e80cc1c720313eb2554ea4ab591533f
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-04 11:40:59 +0100

    Fix card styling

--- my-app/components/card.js
+++ my-app/components/card.js
@@ -2,28 +2,19 @@ import { useState } from "react";
 import styles from "../styles/Card.module.css";
 
 export default function Card(props) {
-  const [nft, setNft] = useState(JSON.parse(props.uri.metadata))
+  const [nft, setNft] = useState(JSON.parse(props.uri.metadata));
   const [nftImage, setNftImage] = useState(() => {
     if (nft?.image) {
-        return nft.image.includes('ipfs') ? 
-        `https://ipfs.io/ipfs/${nft.image.split('ipfs://')[1]}` :
-          nft.image.split('\\')[0];
-        }
+      return nft.image.includes("ipfs")
+        ? `https://ipfs.io/ipfs/${nft.image.split("ipfs://")[1]}`
+        : nft.image.split("\\")[0];
     }
-  )
+  });
 
   return (
     <section className={styles.cardContainer}>
-      {nft?.name ? (
-        <h1>{nft.name}</h1>
-      ) : (
-        <h1>No NFT title can be shown.</h1>
-      )}
-      {nftImage ? (
-        <img src={nftImage} />
-      ) : (
-        <p>No NFT image can be shown.</p>
-      )}
+      {nft?.name ? <h1>{nft.name}</h1> : <h1>No NFT title can be shown.</h1>}
+      {nftImage ? <img src={nftImage} /> : <p>No NFT image can be shown.</p>}
     </section>
   );
 }
